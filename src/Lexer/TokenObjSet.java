b26578708495694f3c4862dfc0f41d3463dded63
angelina-tsuboi <angelina.t1832@gmail.com>
2020-12-07 11:01:02 -0800

    basic token object complete

--- src/Lexer/TokenObjSet.java
+++ src/Lexer/TokenObjSet.java
@@ -7,17 +7,56 @@ import java.util.regex.Pattern;
 public enum TokenObjSet {
   // Token types cannot have underscores
   
+  //basic data types
   NUMBER("(?<=\\s|^)\\d+(?=\\s|$)"),
   DECIMAL("\\d+(\\.\\d{1,2})?"),
   STRING("\"[^\"]*\""),
   BOOLEAN("true(?=[^_A-Za-z])|false(?=[^_A-Za-z])"),
+  WHITESPACE("[ \t\f\r\n]+"),
+  
+  //operators
   OPADD("[+]"),
   OPSUBTRACT("[-]"),
   OPMULTIPLY("[*]"),
   OPDIVIDE("[/]"),
+  OPMODULO("[%]"),
+  OPEQUALTO("=="),
+  OPNOTEQUALTO("!="),
+  OPGREATERTHAN(">"),
+  OPLESSTHAN("<"),
+  OPGREATERTHANEQUALTO(">="),
+  OPLESSTHANEQUALTO("<="),
+  OPAND("&&"),
+  OPOR("||"),
+  OPNOT("!"),
   
+  //punctuators
+  PUNCQUESTION("[?]"),
+  PUNCCOLON("[:]"),
+  PUNCSEMICOLON("[;]"),
+  PUNCPERIOD("[.]"),
+  PUNCCOMMA("[,]"),
+  PUNCLEFTPAREN("[(]"),
+  PUNCRIGHTPAREN("[)]"),
+  PUNCLEFTCURLY("[{]"),
+  PUNCRIGHTCURLY("[}]"),
+  PUNCLEFTSQUARE("["),
+  PUNCRIGHTSQUARE("]"),
   
-  WHITESPACE("[ \t\f\r\n]+");
+  //keywords
+  KEYIF("if(?=[^_A-Za-z])"),
+  KEYFOR("for(?=[^_A-Za-z])"),
+  KEYWHILE("while(?=[^_A-Za-z])"),
+  KEYBLOCK("block(?=[^_A-Za-z])"),
+  KEYEMIT("emit(?=[^_A-Za-z])"),
+  KEYTASK("task(?=[^_A-Za-z])"),
+  KEYNULL("null(?=[^_A-Za-z])"),
+  
+  //identifiers
+  IDENTCAPITALIZED("[A-Z][_A-Za-z]*(?=[^_A-Za-z])"),
+  IDENTLOWERCASE("[a-z][_A-Za-z]*(?=[^_A-Za-z])"),
+  IDENTUNDERSCORE("_[_A-Za-z]*(?=[^_A-Za-z])");
+ 
   
 
   public final String pattern;
@@ -26,17 +65,3 @@ public enum TokenObjSet {
     this.pattern = pattern;
   }
 }
-
-//OP_ADD("(?<=^\\s*)\\+"),
-//OP_SUBTRACT("(?<=^\\s*)\\+"),
-//TEXT_LOWERCASE("(?<=^\\s*)[a-z][_A-Za-z]*(?=[^_A-Za-z])"),
-//TEXT_UPPERCASE("(?<=^\\s*)_[_A-Za-z]*(?=[^_A-Za-z])"),
-//TEXT_UNDERSCORE("(?<=^\\s*)_[_A-Za-z]*(?=[^_A-Za-z])")
-//STRING("(?<=^\\s*)'([^'\\\\]|\\\\.)*'"),
-//DECIMAL("\\d+\\.?\\d*"),
-//BOOLEAN("(?<=^\\s*)true(?=[^_A-Za-z])|false(?=[^_A-Za-z])"),
-
-//OP_PLUS("[+]"),
-//OP_MINUS("[-]"),
-//OP_MULTIPLY("[*]"),
-//OP_DIVIDE("[/]"),
